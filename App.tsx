de5fa0d2ccbf6ac88346fbf44c62b45cba7a7e5a
SooHwanIT <ghks3691@naver.com>
2024-08-17 09:41:36 +0900

    네비 수정

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "react-native-vector-icons/Ionicons"
 import GroupTimerScreen from './src/screens/GroupTimerScreen';
 import SingleTimerScreen from './src/screens/SingleTimerScreen';
-import TemporaryTimerScreen from './src/screens/TemporaryTimerScreen';
+import OptionTimerScreen from './src/screens/OptionTimerScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Single" component={SingleTimerScreen} />
-        <Tab.Screen name="Group" component={GroupTimerScreen} />
-        <Tab.Screen name="Temporary" component={TemporaryTimerScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="싱글"
+                    component={SingleTimerScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="timer-outline" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="그룹"
+                    component={GroupTimerScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="people-outline" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="옵션"
+                    component={OptionTimerScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="settings-outline" size={size} color={color} />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
